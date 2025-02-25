23634fff6d73de9d3ab11742906c34fd844cff54
Nolann Dubos <nolann.dubos@epitech.eu>
2025-02-25 15:27:23 +0100

    add all rules

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,41 @@ module Main (main) where
 
 import Lib (printRule)
 
+import System.Environment (getArgs)
+import System.Exit (ExitCode (ExitFailure), exitWith)
+
+data Conf = Conf
+  { rule :: Maybe Int
+  , start :: Int
+  , nLines :: Maybe Int
+  , window :: Int
+  , move :: Int
+  }
+
+defaultConf :: Conf
+defaultConf =
+  Conf
+    { rule = Nothing
+    , start = 0
+    , nLines = Nothing
+    , window = 80
+    , move = 0
+    }
+
+getOpts :: Conf -> [String] -> Maybe Conf
+getOpts conf [] = Just conf
+getOpts conf ("--rule" : v : args) = getOpts (conf{rule = Just (read v)}) args
+getOpts conf ("--start" : v : args) = getOpts (conf{start = read v}) args
+getOpts conf ("--lines" : v : args) = getOpts (conf{nLines = Just (read v)}) args
+getOpts conf ("--window" : v : args) = getOpts (conf{window = read v}) args
+getOpts conf ("--move" : v : args) = getOpts (conf{move = read v}) args
+getOpts _ _ = Nothing
+
 main :: IO ()
-main = printRule 30 20
+main = do
+  args <- getArgs
+  case getOpts defaultConf args of
+    Just conf -> case rule conf of
+      Just r -> printRule r (start conf) (nLines conf) (window conf) (move conf)
+      Nothing -> exitWith (ExitFailure 84)
+    Nothing -> exitWith (ExitFailure 84)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,6 @@
 module Lib (
   printRule,
+  readInt,
 ) where
 
 import Data.Char (isDigit)
@@ -19,10 +20,37 @@ applyRule :: Int -> (Int, Int, Int) -> Int
 applyRule rule (l, c, r) =
   fromJust (readInt [printf "%08b" rule !! (7 - (l * 4 + c * 2 + r))])
 
+nextGeneration :: Int -> [Int] -> [Int]
+nextGeneration rule cells = nextGen (head cells : cells ++ [last cells])
+ where
+  nextGen (x : y : z : rest) = applyRule rule (x, y, z) : nextGen (y : z : rest)
+  nextGen _ = []
+
+createCell :: Int -> [Int] -> [[Int]]
+createCell rule cells = iterate (nextGeneration rule) cells
+
+createGen0 :: Int -> [Int]
+createGen0 size = replicate (size * 2) 0 ++ [1] ++ replicate (size * 2) 0
+
 display :: Int -> Char
 display 1 = '*'
 display 0 = ' '
 display _ = ' '
 
-printRule :: Int -> Int -> IO ()
-printRule r = printf "Rule %d\n" r
+loop :: [[Int]] -> IO ()
+loop [] = putStrLn ""
+loop (x : xs) = putStrLn (map display x) >> loop xs
+
+cutPart :: Int -> Int -> [Int] -> [Int]
+cutPart window move line =
+  take window (drop (window * 2 - div window 2 - move) line)
+
+getGenerations :: Int -> Int -> Int -> [[Int]]
+getGenerations rule window move =
+  map (cutPart window move) (createCell rule (createGen0 window))
+
+printRule :: Int -> Int -> Maybe Int -> Int -> Int -> IO ()
+printRule rule start nlines window move =
+  case nlines of
+    Nothing -> loop (drop start (getGenerations rule window move))
+    Just n -> loop (take n (drop start (getGenerations rule window move)))
